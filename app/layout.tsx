0a0f819403b19e1fdeb9a46cff03e89be99490b8
mosmn <ithinkisee8@gmail.com>
2024-06-28 04:40:18 +0800

    chore: Update layout to include ThemeProvider for theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mohamed's Tech Blog - Coding, Web Development, AI, and More",
+  title: "Osman's Tech Blog - Coding, Web Development, AI, and More",
   description: "Explore in-depth articles on coding, web development, AI, and the latest in tech. Join John Doe's journey into the world of technology.",
 };
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
