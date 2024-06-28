178d7452aa25db7726710e8db9c7c70176536dfe
mosmn <ithinkisee8@gmail.com>
2024-06-28 17:06:33 +0800

    chore: Optimize page layout and remove unnecessary whitespace

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import  {ModeToggle} from "@/components/theme-toggle";
 import {Logo} from "@/components/logo";
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
@@ -16,6 +17,7 @@ export default function Home() {
       <Button >Read the Blog</Button>
       <ModeToggle />
       <Logo state="static"/>
+
     </main>
   );
 }
