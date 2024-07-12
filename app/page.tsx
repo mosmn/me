9f6ddca7cf99d99378b9a82c624a765c4f49a2a0
mosmn <ithinkisee8@gmail.com>
2024-07-12 09:20:10 +0800

    refactor: Update page component structure
    Refactor the page component to improve code organization and readability. Replace the existing content with separate sections for the hero, commercial projects, and best writings. This change enhances the maintainability and scalability of the codebase.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import  {ModeToggle} from "@/components/theme-toggle";
-import {Logo} from "@/components/logo";
+import { HeroSection } from "@/components/sections/HeroSection";
+import { CommercialProjects } from "@/components/sections/CommercialProjects";
+import { BestWritings } from "@/components/sections/BestWritings";
 
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to Osman Tech Blog
-      </h1>
-      <p className="text-center">
-        Explore in-depth articles on coding, web development, AI, and the latest
-        in tech. Join John Doe journey into the world of technology.
-      </p>
-      <Button >Read the Blog</Button>
-      <ModeToggle />
-      <Logo state="static"/>
-
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <HeroSection />
+      <CommercialProjects />
+      <BestWritings />
+      {/*
+      <TechStack />
+      <CommercialProjects />
+      <BestWritings /> */}
     </main>
   );
 }
